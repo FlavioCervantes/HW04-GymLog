3fdbdfa82a2590e282cd4b6e9a5daca6f4dad304
FlavioCeravntes <fcervantes@csumb.edu>
2025-04-09 01:06:49 -0700

    Video2-Complete || Wire up display || View bindings || Scrolling is working || <3

--- app/src/main/java/com/example/test_hw04_gymlog/MainActivity.java
+++ app/src/main/java/com/example/test_hw04_gymlog/MainActivity.java
@@ -6,6 +6,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -15,14 +17,63 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.test_hw04_gymlog.databinding.ActivityMainBinding;
 
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    String mExercise = "";
+    double mWeight = 0.0;
+    int mReps = 0;
+    ActivityMainBinding binding;
+
+    private static final String TAG = "DAC GYMLOG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+// scrollable
+        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+        // Set up the toolbar
+        binding.logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInformationFromDisplay();
+                updateDisplay();
+
+            }
+        });
+
+    }
+
+    private void updateDisplay() {
+        getInformationFromDisplay();
+     String currentInfo = binding.logDisplayTextView.getText().toString();
+    String newDisplay = String.format(Locale.US,"Exercise: %s%nWeight:%.2f%nReps:%d%n     %n%s", mExercise, mWeight, mReps,currentInfo);
+
+    binding.logDisplayTextView.setText(newDisplay);
+    }
+
+    private void getInformationFromDisplay() {
+        mExercise = binding.exerciseInputEditText.getText().toString();
+
+        try {
+            mWeight = Double.parseDouble(binding.weightInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(TAG, "Error reading value from Weight edit text");
+        }
+
+        try {
+            mReps = Integer.parseInt(binding.repInputEditText.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(TAG, "Error reading value from reps edit text");
+        }
     }
 }
